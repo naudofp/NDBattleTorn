09e7629e52fac6e1ded2ee31f928ca24d5ed890c
Fellipe Naudo <felipenaudof@gmail.com>
2024-12-07 22:26:23 -0300

    #0003 - Solving bugs

---
+++ forge16/src/main/java/com/naudodev/ndbattletorn/forge/service/BattleInfoService.java
@@ -0,0 +1,11 @@
+package com.naudodev.ndbattletorn.forge.service;
+
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.api.platform.Messageable;
+
+public class BattleInfoService {
+
+    public static void getBattleLog(ForgeEnvyPlayer player, Messageable<?> sender) {
+
+    }
+}
